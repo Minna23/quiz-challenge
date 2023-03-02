94dd2dc4afa4cd8634a83e08b2450908c420d8c6
Minna23 <andrea.teiermayer@gmail.com>
2023-03-02 21:33:23 +0000

    built file structure

---
+++ assets/js/questions.js
@@ -0,0 +1,15 @@
+let questions = [{
+    "title": "Common Data Types DO NOT include:",
+    "choices": ["Strings", "Booleans", "Alerts", "Numbers"],
+    "answer": "Alerts"
+},
+{
+    "title": "The Condition in an If/Else statement is enclosed with:",
+    "choices": ["quotes", "curly brackets", "parenthesis", "square bracket"],
+    "answer": "parenthesis" 
+},
+{
+    "title": "Arrays in JavaScript used to store:",
+    "choices": ["numbers and strings", "other arrays", "Booleans", "all of the above"],
+    "answer": "all of the above"
+}]
